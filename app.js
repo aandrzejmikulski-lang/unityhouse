e3fde7c6ef35ab53d737c792300cb056f1d167cc
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-13 22:01:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// Supabase client
-const client = supabase.createClient(
-  "https://vswonxgsaqnhzsmzexzh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
-);
-
 // Elementy DOM
 const authCard = document.getElementById("auth-card");
 const mainCard = document.getElementById("main-card");
