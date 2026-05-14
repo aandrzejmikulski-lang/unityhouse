43f85b83586a37145764b27c5a749ffb892d13ac
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-14 13:06:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // =====================================
 
 const client = supabase.createClient(
-  "https://YOUR_PROJECT.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
+  "https://vswonxgsaqnhzsmzexzh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
 );
 
 // =====================================
