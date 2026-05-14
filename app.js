6c67d13d2124dcb9231a486b5bd9bbeffe345240
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-14 17:01:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ console.log("APP START");
 
 const client = supabase.createClient(
   "https://vswonxgsaqnhzsmzexzh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk",
+  {
+    auth: {
+      persistSession: false
+    }
+  }
 );
 
 console.log("Supabase client:", client);
@@ -26,7 +31,7 @@ document.getElementById("btnLogin").addEventListener("click", async () => {
 
   console.log("client.auth:", client.auth);
 
-  const { data, error } = await client.auth.signIn({
+  const { data, error } = await client.auth.signInWithPassword({
     email: email,
     password: password
   });
