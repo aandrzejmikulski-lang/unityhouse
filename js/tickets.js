197d2083519c16aac38dfa1a91bd69bb52e8b4ad
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-15 09:58:45 +0200

    Update tickets.js

--- js/tickets.js
+++ js/tickets.js
@@ -1,1 +1,13 @@
+// tickets.js
 
+function initTickets() {
+  // np. event listenery do tworzenia zgłoszeń
+}
+
+async function createTicket() {
+  // tworzenie zgłoszenia
+}
+
+async function loadTickets() {
+  // pobieranie zgłoszeń
+}
