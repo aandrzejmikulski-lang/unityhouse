32a0ebd4ff92e1224ba50bf14faa99e8da9c6040
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-15 09:58:18 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,20 @@
+// auth.js
 
+function initAuth() {
+  document.getElementById("btnLogin")?.addEventListener("click", loginUser);
+  document.getElementById("btnRegister")?.addEventListener("click", registerUser);
+  document.getElementById("btnLogout")?.addEventListener("click", logoutUser);
+}
+
+async function registerUser() {
+  // tu wkleimy Twój kod rejestracji
+}
+
+async function loginUser() {
+  // tu wkleimy logowanie
+}
+
+async function logoutUser() {
+  await client.auth.signOut();
+  switchPanel("loginPanel");
+}
