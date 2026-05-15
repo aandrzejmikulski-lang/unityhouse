08bca25c78bc2d8110c1ac69f0c5020ddcef4975
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-15 09:58:30 +0200

    Update profiles.js

--- js/profiles.js
+++ js/profiles.js
@@ -1,1 +1,14 @@
+// profiles.js
+
+function initProfiles() {
+  // np. ładowanie listy profili admina
+}
+
+async function loadProfiles() {
+  // pobieranie profili
+}
+
+async function approveUser(id) {
+  // akceptacja użytkownika
+}
 
