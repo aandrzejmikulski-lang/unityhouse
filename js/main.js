c4a5ea0c7b28203ac9de17b997b73018a7178d12
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-15 09:58:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// main.js
+
+// Inicjalizacja Supabase
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Start aplikacji
+document.addEventListener("DOMContentLoaded", () => {
+  initUI();
+  initAuth();
+  initTickets();
+  initProfiles();
+});
 
