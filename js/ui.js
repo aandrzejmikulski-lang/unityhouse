a9c8bd4844efdfe0b3aa6ee97f0873805d4f4ba7
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-15 09:58:59 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,15 @@
+// ui.js
 
+function initUI() {
+  // np. obsługa menu, modali
+}
+
+function showMessage(element, text, type = "info") {
+  element.textContent = text;
+  element.className = type;
+}
+
+function switchPanel(panelId) {
+  document.querySelectorAll(".panel").forEach(p => p.style.display = "none");
+  document.getElementById(panelId).style.display = "block";
+}
