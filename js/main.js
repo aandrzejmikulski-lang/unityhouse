1d37c8ac7815535e7b6e2bf5b0d680223a73e6c2
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-16 09:30:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const client = supabase.createClient(
   "https://vswonxgsaqnhzsmzexzh.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk",
-  { auth: { persistSession: false } }
+  { auth: { persistSession: true, autoRefreshToken: true } }
 );
 
 // =======================================
