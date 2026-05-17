b32d93a3a116fd5af0d7378d9a22dca0d2090cd5
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-17 14:48:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ window.addEventListener("DOMContentLoaded", async () => {
   // 1. INICJALIZACJA SUPABASE
   // ---------------------------------------------
   App.supabase = supabase.createClient(
-    "https://YOUR-PROJECT.supabase.co",
-    "YOUR-PUBLIC-ANON-KEY"
+    "https://vswonxgsaqnhzsmzexzh.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
   );
 
   console.log("Supabase initialized");
