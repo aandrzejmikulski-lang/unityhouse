f12e6d7c5ec138d5bb7b81e1db7b6cef45fb917d
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-17 16:26:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,10 +10,16 @@ window.addEventListener("DOMContentLoaded", async () => {
   // ---------------------------------------------
   // SUPABASE
   // ---------------------------------------------
-  App.supabase = supabase.createClient(
-    "https://vswonxgsaqnhzsmzexzh.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
-  );
+App.supabase = supabase.createClient(
+  "https://vswonxgsaqnhzsmzexzh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk",
+  {
+    auth: {
+      storage: window.sessionStorage,
+      persistSession: true
+    }
+  }
+);
 
   console.log("Supabase initialized");
 
