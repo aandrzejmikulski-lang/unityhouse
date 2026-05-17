a3eb669c56ec509d1b8dad38a85aa78789c49813
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-17 15:13:19 +0200

    Update Supabase client initialization with new key

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", async () => {
   // ---------------------------------------------
   App.supabase = supabase.createClient(
     "https://vswonxgsaqnhzsmzexzh.supabase.co",
-    "TWÓJ_KEY_TUTAJ"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk"
   );
 
   console.log("Supabase initialized");
