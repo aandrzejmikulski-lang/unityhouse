977d2e7cc2417a8d65b20ad2e0ab1698574da5d1
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-18 12:53:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,15 +11,23 @@ window.addEventListener("DOMContentLoaded", async () => {
   // SUPABASE
   // ---------------------------------------------
   App.supabase = supabase.createClient(
-    "https://vswonxgsaqnhzsmzexzh.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk",
-    {
-      auth: {
-        storage: window.sessionStorage,
-        persistSession: true
+  "https://vswonxgsaqnhzsmzexzh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzd29ueGdzYXFuaHpzbXpleHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjQ2OTYsImV4cCI6MjA5NDI0MDY5Nn0.mBBGMqqSRQgtM9k0aOH1Nl3WdNRj3Xj9nY6TqJgsepk",
+  {
+    auth: {
+      storage: window.sessionStorage,
+      persistSession: true,
+      autoRefreshToken: true
+    },
+    global: {
+      headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+        Authorization: `Bearer ${sessionStorage.getItem("sb-access-token")}`
       }
     }
-  );
+  }
+);
+
 
   console.log("Supabase initialized");
 
